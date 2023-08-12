596b6fcb21295652472efc4efb65052bfdb1a187
Indranjana <112919974+IndranjanaChatterjee@users.noreply.github.com>
2023-08-12 09:23:09 +0530

    Add files via upload

---
+++ swapList.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<malloc.h>
+	typedef struct node
+	{
+		int value;
+		struct node *next;
+	}node;
+	int main()
+	{
+		node *ptr,*s;
+		s=NULL;
+		int n;
+		printf("Enter the number of nodes:");
+		scanf("%d",&n);
+		int i,val;
+		for(i=0;i<n;i++)
+		{
+			ptr=(node *)(malloc(sizeof(node)));
+			printf("Enter the value:");
+			scanf("%d",&val);
+			ptr->value=val;
+			ptr->next=NULL;
+			if(s==NULL)
+			{
+				s=ptr;
+			}
+			else
+			{
+				ptr->next=s;
+				s=ptr;
+			}
+		}
+		ptr=s;
+		printf("\nWithout Reversing:");
+		while(ptr!=NULL)
+		{
+			printf("\n[%p]{%d %p}",ptr->next,ptr->value,ptr);
+			ptr=ptr->next;
+		}
+		ptr=s;
+		int swap;
+		while(ptr!=NULL && ptr->next!=NULL)
+		{
+			swap=ptr->value;//Swapping part
+			ptr->value=ptr->next->value;//Swapping part
+			ptr->next->value=swap;//Swapping part
+			ptr=ptr->next->next;//Going next to next
+		}
+		printf("\nAfter Swaping the list:");
+		ptr=s;
+		while(ptr!=NULL)
+		{
+			printf("\n[%p]{%d %p}",ptr->next,ptr->value,ptr);
+			ptr=ptr->next;
+		}
+		
+	}
